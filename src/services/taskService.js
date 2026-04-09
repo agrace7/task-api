701622e99ff7528187088f00975eb5603aebef4d
Amanda <agrace7@charlotte.edu>
2026-04-09 17:38:21 -0400

    Add completed query filtering to tasks endpoint

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,7 +1,13 @@
 import * as taskRepository from '../repositories/taskRepo.js';
 
-export async function getAllTasks() {
-  return taskRepository.findAll();
+export async function getAllTasks(completed) {
+  let completedFilter;
+  if (completed === undefined) {
+    completedFilter = undefined;
+  } else {
+    completedFilter = completed === 'true';
+  }
+  return taskRepository.findAll(completedFilter);
 }
 
 export async function createTask(newTask) {
